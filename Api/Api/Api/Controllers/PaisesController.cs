6ff238b8e62badefb5e44f435bba28f4c1463e82
lorddeimos3051 <lorddeimos3051@gmail.com>
2020-11-11 23:32:32 -0600

    api y script bd
    Adicion de api y script de creación de base de datos.

---
+++ Api/Api/Api/Controllers/PaisesController.cs
@@ -0,0 +1,36 @@
+﻿using ConectaDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Api.Controllers
+{
+    public class PaisesController : ApiController
+    {
+        private PaisesEntities dbContext = new PaisesEntities();
+
+        //Visualiza todos los registros (api/paises)
+        [HttpGet]
+        public IEnumerable<Pais> Get()
+        {
+            using (PaisesEntities paisesentities = new PaisesEntities())
+            {
+                return paisesentities.Pais.ToList();
+            }
+        }
+
+        //Visualiza todos los registros (api/paises)
+        [HttpGet]
+        public Pais Get(int idPais)
+        {
+            using (PaisesEntities paisesentities = new PaisesEntities())
+            {
+                return paisesentities.Pais.FirstOrDefault(e => e.codPais == idPais);
+            }
+        }
+
+    }
+}
---
+++ Api/Api/Api/Controllers/RegionesController.cs
@@ -0,0 +1,33 @@
+﻿using ConectaDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Api.Controllers
+{
+    public class RegionesController : ApiController
+    {
+        //Visualiza todos los registros (api/regiones)
+        [HttpGet]
+        public IEnumerable<Region> Get()
+        {
+            using (RegionesEntities regionesentities = new RegionesEntities())
+            {
+                return regionesentities.Region.ToList();
+            }
+        }
+
+        //Visualiza todos los registros (api/regiones)
+        [HttpGet]
+        public Region Get(int id)
+        {
+            using (RegionesEntities regionesentities = new RegionesEntities())
+            {
+                return regionesentities.Region.FirstOrDefault(e => e.codRegion == id);
+            }
+        }
+    }
+}
